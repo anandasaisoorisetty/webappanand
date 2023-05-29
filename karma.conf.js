3a73ba38b17ed577745474dc9a0cb979909dbd74
anandasaisoorisetty <anandasaisoorisetty@gmail.com>
2023-05-30 03:05:10 +0530

    Jenkins file updated

---
+++ karma.conf.js
@@ -0,0 +1,33 @@
+module.exports = function (config) {
+    config.set({
+        frameworks: ['jasmine', '@angular/cli'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('@angular/cli/plugins/karma')
+        ],
+        files: [
+            { pattern: './src/test.ts', watched: false }
+        ],
+        preprocessors: {
+            './src/test.ts': ['@angular/cli']
+        },
+        mime: {
+            'text/x-typescript': ['ts', 'tsx']
+        },
+        browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox', 'Puppeteer'],
+        customLaunchers: {
+            Puppeteer: {
+                base: 'ChromeHeadless',
+                flags: [
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox',
+                    '--disable-dev-shm-usage'
+                ],
+                debug: false
+            }
+        },
+
+        singleRun: true
+    });
+};
